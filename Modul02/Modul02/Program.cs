97b8cebda81e7a469986033417bf6bd8c57cd277
todormirchev <mirchev.todor@gmail.com>
2017-03-22 04:55:11 +0200

    модул 02. Видео 01 - Добавяне на коментари

---
+++ Modul02/Modul02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Modul02
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			
+
+			//Дефиниране на променливи
+
+
+			//Събиране на числа
+
+
+			//Писане в конзола
+
+		}
+	}
+}
