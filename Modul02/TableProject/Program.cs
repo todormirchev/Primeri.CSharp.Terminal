2fb1a6b6f38500bbd0e58164d58aefc4b7b71c36
todormirchev <mirchev.todor@gmail.com>
2017-03-31 13:43:38 +0300

    Модул 02, Видео 08

--- Modul02/TableProject/Program.cs
+++ Modul02/TableProject/Program.cs
@@ -6,19 +6,40 @@ namespace TableProject
 	{
 		public static void Main(string[] args)
 		{
-			String[] row = new String[3];
-			row[0] = "Ред 1";
-			row[1] = "Ред 2";
-			row[2] = "Ред 3";
+			//Дефиниране на таблица
+			string[,] table = new string[2, 3];
 
-			Console.WriteLine("Масив row: " + row[0] + ", " + row[1] + ", " + row[2] + "\n");
+			//Въвеждане на първи ред
+			table[0, 0] = "Тодор"; table[0, 1] = "Мирчев"; table[0, 2] = "30";
 
-			string[] parse = "1,2,3,4,5,6,7,8,9".Split(',');
-			Console.WriteLine("Броят на масива:\n" + "1,2,3,4,5,6,7,8,9\n\ne:" + parse.Length);
 
-			string list1 = string.Join(";", parse);
-			Console.WriteLine("Новият Стринг е:\n" + list1 + "\n\n");
+			//Въвеждане на втори ред
+			table[1, 0] = "Мирчо"; table[1, 1] = "Тодоров"; table[1, 2] = "31";
+
+
+			//Печат
+			Console.Write("Кой ред искате да видите: ");
+			int _index = Convert.ToInt32(Console.ReadLine()) - 1;
+
+			Console.WriteLine((table[_index,0])[0] + ". " + table[_index,1] + ", " + table[_index,2] + " г.");
 
 		}
 	}
 }
+
+
+
+
+
+//String[] row = new String[3];
+//row[0] = "Ред 1";
+//row[1] = "Ред 2";
+//row[2] = "Ред 3";
+
+//Console.WriteLine("Масив row: " + row[0] + ", " + row[1] + ", " + row[2] + "\n");
+
+//string[] parse = "1,2,3,4,5,6,7,8,9".Split(',');
+//Console.WriteLine("Броят на масива:\n" + "1,2,3,4,5,6,7,8,9\n\ne:" + parse.Length);
+
+//string list1 = string.Join(";", parse);
+//Console.WriteLine("Новият Стринг е:\n" + list1 + "\n\n");
