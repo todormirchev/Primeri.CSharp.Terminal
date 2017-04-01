c15c11a4ea2e387e4796e449beea238ba337f45f
todormirchev <mirchev.todor@gmail.com>
2017-04-01 12:07:49 +0300

    Модул 03, Видео 05 - Булеви операции

---
+++ Modul03/BoolExplore/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BoolExplore
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			//bool _b = true;
+
+			//bool _test = 5 > 10;
+
+			int[] _i = new int[] { 4, 3, 2 };
+
+			int _ui = Convert.ToInt32(Console.ReadLine());
+
+			bool _check01 = _ui <= _i.Length;
+			bool _check02 = _ui > 0;
+
+			bool _exp01 = _check01 && _check02;
+			bool _exp02 = _check01 || _check02;
+			bool _exp03 = !_check01;
+
+
+			Console.WriteLine("Логическо И:" + _exp01.ToString());
+			Console.WriteLine("Логическо ИЛИ:" + _exp02.ToString());
+			Console.WriteLine("Отрицание на " + _check01.ToString() + ": " + _exp03);
+
+			//Console.WriteLine(_i[_ui - 1]);
+		}
+	}
+
+}
--- Modul03/Modul03/Program.cs
+++ Modul03/Modul03/Program.cs
@@ -11,9 +11,23 @@ namespace Modul03
 			Console.Write("> a ="); a = Convert.ToDouble(Console.ReadLine());
 			Console.Write("> b ="); b = Convert.ToDouble(Console.ReadLine());
 
+			//Форматиране с дименсия
 			string _format = "#0.00 m3";
 
-			Console.WriteLine("\n\n>a / b = " + (a / b).ToString(_format));
+			//Степенуване
+			c = Math.Pow(a, b);
+			Console.WriteLine("\n\n>a ^ b = " + c.ToString(_format));
+
+			//Коренуване
+			c = Math.Sqrt(b);
+			Console.WriteLine("\n\n>b ^ 1/2 = " + c.ToString(_format));
+
+			//Закръгления
+			c = Math.Round(a/b);
+			Console.WriteLine("\n\n>(a / b) = " + c.ToString());
+
+
+
 		}
 	}
 }
