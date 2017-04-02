dc78e867259cf6d3db942b3e58795cde777b323d
todormirchev <mirchev.todor@gmail.com>
2017-04-02 13:08:28 +0300

    Модул03, Видео 06 - Работа с условн оператори if

--- Modul03/BoolExplore/Program.cs
+++ Modul03/BoolExplore/Program.cs
@@ -14,17 +14,36 @@ namespace BoolExplore
 
 			int _ui = Convert.ToInt32(Console.ReadLine());
 
-			bool _check01 = _ui <= _i.Length;
-			bool _check02 = _ui > 0;
+			//bool _check01 = _ui <= _i.Length;
+			//bool _check02 = _ui > 0;
 
-			bool _exp01 = _check01 && _check02;
-			bool _exp02 = _check01 || _check02;
-			bool _exp03 = !_check01;
+			//bool _exp01 = _check01 && _check02;
+			//bool _exp02 = _check01 || _check02;
+			//bool _exp03 = !_check01;
 
 
-			Console.WriteLine("Логическо И:" + _exp01.ToString());
-			Console.WriteLine("Логическо ИЛИ:" + _exp02.ToString());
-			Console.WriteLine("Отрицание на " + _check01.ToString() + ": " + _exp03);
+			//Console.WriteLine("Логическо И:" + _exp01.ToString());
+			//Console.WriteLine("Логическо ИЛИ:" + _exp02.ToString());
+			//Console.WriteLine("Отрицание на " + _check01.ToString() + ": " + _exp03);
+
+
+			if (_ui > 0 && _ui <= _i.Length)
+			{
+				Console.WriteLine(_i[_ui - 1]);
+			}
+			else {
+				if (_ui > _i.Length)
+				{
+					Console.WriteLine("Индекса е прекалено голям\n\n\n");
+				}
+				if (_ui <= 0)
+				{
+					Console.WriteLine("Индекса е прекалено Малък\n\n\n");
+				}
+
+			}
+
+
 
 			//Console.WriteLine(_i[_ui - 1]);
 		}
