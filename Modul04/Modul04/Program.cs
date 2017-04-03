ba0ac929c23e9fd53b01c1b7cd761840c1ab4d66
todormirchev <mirchev.todor@gmail.com>
2017-04-03 21:40:04 +0300

    Модул 04, Видео 05 - Цикъл Do..While

--- Modul04/Modul04/Program.cs
+++ Modul04/Modul04/Program.cs
@@ -23,15 +23,41 @@ namespace Modul04
 
 
 			//Тяло на таблица
-			for (int i = 0; i < _table.Length/3; i++ )
-			{
-				for (int j = 0; j < 3; j++)
-				{
-					Console.Write(_table [i,j].PadRight(12));
-				}
-				Console.WriteLine();
 
-			}
+			//Пример с for
+			//for (int i = 0; i < _table.Length/3; i++ )
+			//{
+			//	for (int j = 0; j < 3; j++)
+			//	{
+			//		Console.Write(_table [i,j].PadRight(12));
+			//	}
+			//	Console.WriteLine();
+
+			//}
+
+
+			//Пример с while
+			//int i = 0;
+			//while (i<_table.Length/3)
+			//{
+			//	Console.WriteLine(_table[i,0].PadRight(12)  + 
+			//	                  _table[i, 1].PadRight(12) + 
+			//	                  _table[i, 2].PadRight(12));
+			//	i++;
+			//}
+
+
+			//Пример с DO...While
+			int i = 0;
+			do
+			{
+				Console.WriteLine(_table[i, 0].PadRight(12) +
+					                  _table[i, 1].PadRight(12) + 
+					                  _table[i, 2].PadRight(12));
+				i++;
+			} while(i<_table.Length/3);
+
+
 
 		}
 	}
