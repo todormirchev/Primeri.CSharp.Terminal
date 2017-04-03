00c7aabb8fda1c58552037e6253b2659508fb5e2
todormirchev <mirchev.todor@gmail.com>
2017-04-03 21:05:46 +0300

    Модул 04, Видео 04 - Цикъл While

---
+++ Modul04/Modul04/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Modul04
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+
+			string[,] _table = {
+				{"Иван", "Георгиев", "19"},
+				{"Васил", "Иванов", "23"},
+				{"Милен", "Георгиев", "58"},
+				{"Цветан", "Борисов", "14"},
+				{"Гергана", "Василева", "25"},
+				{"Ивана", "Христова", "21"}
+
+			};
+
+			//Антетка на таблица
+			Console.WriteLine("Име".PadRight(12) + "Фамилия".PadRight(12) + "Години".PadRight(12) );
+			Console.WriteLine("===============================");
+
+
+			//Тяло на таблица
+			for (int i = 0; i < _table.Length/3; i++ )
+			{
+				for (int j = 0; j < 3; j++)
+				{
+					Console.Write(_table [i,j].PadRight(12));
+				}
+				Console.WriteLine();
+
+			}
+
+		}
+	}
+}
