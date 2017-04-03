6f785281bb1d4d956f0410e170c4302f5288112f
todormirchev <mirchev.todor@gmail.com>
2017-04-03 21:52:50 +0300

    Модул 04, Видео 06 - Примери Do..While

---
+++ Modul04/ListExplore/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ListExplore
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			//Дефиниции
+			string _userinput = "";
+
+
+
+			//Потребителски вход
+			do
+			{
+				Console.Write("? ");
+				_userinput = Console.ReadLine();
+
+			} while (_userinput!= "exit");
+
+
+		}
+	}
+}
