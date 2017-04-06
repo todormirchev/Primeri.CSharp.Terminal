daec3e372acd17cfac7d56a38595a1f7d10a04cf
todormirchev <mirchev.todor@gmail.com>
2017-04-06 20:50:43 +0300

    Модул 04, Видео 09 - Примери Foreach

--- Modul04/ListExplore/Program.cs
+++ Modul04/ListExplore/Program.cs
@@ -43,14 +43,13 @@ namespace ListExplore
 				//Преглед на листа
 				if (_userinput.Contains("show"))
 				{
-					Console.Write("List: ");
-					for (int i = 0; i < _list.Count; i++)
+					Console.Write("Списък: ");
+
+					foreach (int value in _list)
 					{
-						Console.Write(_list[i]);
-
-						if (i != _list.Count - 1) Console.Write(", ");
-
+						Console.Write(value.ToString() + ", ");
 					}
+
 					Console.WriteLine("\n");
 				}
 
