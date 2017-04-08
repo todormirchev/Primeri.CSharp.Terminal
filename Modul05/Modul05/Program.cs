d07aaa9a938c9f8cbe166e972d38d77c6fcb2a7a
todormirchev <mirchev.todor@gmail.com>
2017-04-08 14:55:24 +0300

    Модул 05, Видео 02 - Използване на Ref

--- Modul05/Modul05/Program.cs
+++ Modul05/Modul05/Program.cs
@@ -14,10 +14,36 @@ namespace Modul05
 			{
 				Console.Write("$"); usercommand = Console.ReadLine();
 				if (usercommand.Contains("cmd01")) cmd01(usercommand);
-				if (usercommand.Contains("cmd02")) Console.WriteLine(cmd02 (usercommand));
+				if (usercommand.Contains("cmd02")) Console.WriteLine(cmd02(usercommand));
 
+				if (usercommand.Contains("cmd03"))
+				{
+					double _a = 0;
+					if (cmd03(usercommand, out _a))
+					{
+						Console.WriteLine("a.a= " + _a.ToString());
+					}
+					else
+					{
+						Console.WriteLine("Командата не е въведена коректно");
+					}
+				}
 
-			} while (usercommand !="exit");
+				if (usercommand.Contains("cmd04"))
+				{
+					double _t = 5, _c = -2;
+
+					if (cmd04(ref _t))
+					{
+						Console.WriteLine(_t);
+					}
+
+					if (cmd04(ref _c))
+					{
+						Console.WriteLine(0);
+					}
+				}
+				} while (usercommand != "exit");
 
 		}
 
@@ -33,8 +59,11 @@ namespace Modul05
 				}
 
 
-			}catch{
-				
+
+			}
+			catch
+			{
+
 			}
 		}
 
@@ -48,7 +77,37 @@ namespace Modul05
 			{
 				_temp = _temp * _temp * _temp;
 			}
-			return _temp; 
+			return _temp;
+		}
+
+
+		public static bool cmd03(string _input, out double _i)
+		{
+			try
+			{
+				string _p = _input.Split(' ')[1];
+				double __p = 0;
+
+				if (double.TryParse(_p, out __p))
+				{
+					_i = __p * __p;
+					return true;
+				}
+
+			}
+			catch
+			{
+
+			}
+			_i = 0;
+			return false;
+			}
+
+		public static bool cmd04(ref double _i)
+		{
+			double _temp = _i;
+			_i = _i* _i;
+				return (_temp > 0);
 		}
 
 
